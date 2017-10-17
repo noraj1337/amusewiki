ef7bff446aadaa9a31d2e62de5d9ec42750c11ae
Marco Pessotto <melmothx@gmail.com>
2017-10-17 11:18:24 +0200

    Add generated files for 39-40 schema upgrade

---
+++ dbicdh/PostgreSQL/upgrade/39-40/001-auto.sql
@@ -0,0 +1,12 @@
+-- Convert schema '/home/melmoth/amw/AmuseWikiFarm/dbicdh/_source/deploy/39/001-auto.yml' to '/home/melmoth/amw/AmuseWikiFarm/dbicdh/_source/deploy/40/001-auto.yml':;
+
+;
+BEGIN;
+
+;
+ALTER TABLE job ADD COLUMN started timestamp;
+
+;
+
+COMMIT;
+
